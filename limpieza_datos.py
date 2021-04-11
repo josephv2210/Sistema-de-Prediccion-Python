de101b329b3356766d0863de1030e935e77f5797
joseph <joseph.2210@hotmail.es>
2021-04-10 19:12:20 -0500

    Comentarios guia

---
+++ limpieza_datos.py
@@ -0,0 +1,24 @@
+
+
+# Borrado de registros inecesarias
+# borramos todos los registros nulos y las filas de la columna total
+
+# Borrado de las columnas inecesarias
+# year
+# area
+# record tipe
+# reliability
+# source year
+# value footnotes
+
+# Cambiar las cabeceras por su traduccion al español
+
+# convercion de datos de string a int
+# Pais Argentina 0, Chile 1, Brazil 2, Mexico 3, Panama 4
+# Sexo male 0, female 1
+# Causa de muerte
+# 
+
+# Generar rangos de edad
+# 0 0-9,1 10-19,2 20-29, 3 30-39,4 40-49,5 50-59,6 60-69, 7 70-79, 8 80-89, 9 90-mas
+
