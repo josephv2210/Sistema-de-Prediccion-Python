8738e80b0397daa2c3f1cedd7f1977ab2827b6de
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-04-10 19:31:12 -0500

    enfermedades

--- limpieza_datos.py
+++ limpieza_datos.py
@@ -22,3 +22,66 @@
 # Generar rangos de edad
 # 0 0-9,1 10-19,2 20-29, 3 30-39,4 40-49,5 50-59,6 60-69, 7 70-79, 8 80-89, 9 90-mas
 
+""" Certain infectious and parasitic diseases
+Intestinal infectious diseases
+Tuberculosis
+Tetanus
+Diphtheria
+Whooping cough
+Meningococcal infection
+Septicaemia
+Acute poliomyelitis
+Measles
+Viral hepatitis
+Human immunodeficiency virus [HIV] disease
+Malaria
+Neoplasms
+Malignant neoplasms
+Malignant neoplasm of lip, oral cavity and pharynx
+Malignant neoplasm of oesophagus
+Malignant neoplasm of stomach
+Malignant neoplasm of colon
+Malignant neoplasm of liver and intrahepatic bile ducts
+Malignant neoplasm of pancreas
+Malignant neoplasm of trachea
+Malignant neoplasm of prostate
+Malignant neoplasm of lymphoid, haematopoietic and related tissue
+Disorders of the blood and blood-forming organs and certain disorders involving the immune mec 
+Anaemias
+Endocrine
+Diabetes mellitus
+Malnutrition
+Mental and behavioural disorders
+Diseases of the nervous system
+Diseases of the circulatory system
+Acute rheumatic fever and chronic rheumatic heart diseases
+Hypertensive diseases
+Ischaemic heart diseases
+Cerebrovascular diseases
+Diseases of arteries, arterioles and capillaries
+Diseases of the respiratory  system
+Influenza
+Pneumonia
+Chronic lower respiratory diseases
+Diseases of the digestive system
+Gastric and duodenal ulcer
+Diseases of the liver
+Diseases of the musculoskeletal system and connective tissue
+Diseases of the genitourinary system
+Disorders of kidney and ureter
+Hyperplasia of prostate
+Certain conditions originating in the perinatal period
+Congenital malformations, deformations and chromosomal abnormalities
+Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified
+All other diseases
+External causes
+Accidents
+Transport accidents
+Falls
+Accidental drowning and submersion
+Exposure to smoke, fire and flames
+Accidental poisoning by and exposure to noxious substances
+Intentional self-harm
+Assault
+All other external causes
+All causes """
