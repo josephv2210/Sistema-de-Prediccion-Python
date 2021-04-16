e69b4050e58e0da89fc23dca0120df1c1c9c79c8
R-Alejandro <AlejandroRHiguera@gmail.com>
2021-04-15 21:08:29 -0500

    comentarios

--- limpieza_datos.py
+++ limpieza_datos.py
@@ -11,12 +11,7 @@ data = pd.read_csv("DataSet_Prediccion.csv")
 
 
 # Borrado de las columnas inecesarias
-# year
-# area
-# record type
-# reliability
-# source year
-# value footnotes
+# year, area, record type, reliability, source year, value footnotes
 data.drop(["Year", "Area","Record Type","Reliability","Source Year","Value Footnotes"], axis=1, inplace=True)
 
 # borramos todos los registros nulos y las filas de la columna total
@@ -36,12 +31,12 @@ data = EliminarFila("Other direct obstetric causes, ICD10", "Cause of death (WHO
 data = EliminarFila("Unknown", "Sex", data)
 
 # Cambiar las cabeceras por su traduccion al espaÃ±ol
-
 cabecera = ["Pais", "Sexo", "Edad", "Causa de Muerte", "Cantidad"]
 data.columns = cabecera
 
-# convercion de datos de string a int
-
+# adecuar las variables categoricoas como pais, sexo, causa de muerte y edad
+#la funcion permite ingresar la lista de los elementos categoricos y remplaza cada nombre por un numero
+#la funcion necesita la columna, el dataset y flag que es para indicar si es una lista o una matriz
 def ConversionCategorizacion(Nombre, Columna, data, flag=True):
     if flag==True:
         for i in range (len(Nombre)):
@@ -49,9 +44,9 @@ def ConversionCategorizacion(Nombre, Columna, data, flag=True):
     else:
         for i in range (len(Nombre)):
             for a in Nombre[i]:
-                # print(a,i)
                 data[Columna] = data[Columna].replace({a:str(i)})
     return data
+
 # Pais Argentina 0, Chile 1, Brazil 2, Mexico 3, Panama 4
 data = ConversionCategorizacion(["Argentina", "Chile", "Brazil", "Mexico", "Panama"], "Pais", data)
 
@@ -130,9 +125,6 @@ data = ConversionCategorizacion(["Male", "Female"], "Sexo", data)
 data = ConversionCategorizacion(["Certain infectious and parasitic diseases, ICD10","Intestinal infectious diseases, ICD10","Tuberculosis, ICD10","Tetanus, ICD10","Diphtheria, ICD10","Whooping cough, ICD10","Meningococcal infection, ICD10","Septicaemia, ICD10","Acute poliomyelitis, ICD10","Measles, ICD10","Viral hepatitis, ICD10","Human immunodeficiency virus [HIV] disease, ICD10","Malaria, ICD10","Neoplasms, ICD10","Malignant neoplasms, ICD10","Malignant neoplasm of lip, oral cavity and pharynx, ICD10","Malignant neoplasm of oesophagus, ICD10","Malignant neoplasm of stomach, ICD10","Malignant neoplasm of colon, rectosigmoid junction, rectum, anus and anal canal, ICD10","Malignant neoplasm of liver and intrahepatic bile ducts, ICD10","Malignant neoplasm of pancreas, ICD10","Malignant neoplasm of trachea, bronchus and lung, ICD10","Malignant neoplasm of prostate, ICD10","Malignant neoplasm of lymphoid, haematopoietic and related tissue, ICD10","Disorders of the blood and blood-forming organs and certain disorders involving the immune mec ICD10","Anaemias, ICD10","Endocrine, nutritional and metabolic diseases, ICD10","Diabetes mellitus, ICD10","Malnutrition, ICD10","Mental and behavioural disorders, ICD10","Diseases of the nervous system, ICD10","Diseases of the circulatory system, ICD10","Acute rheumatic fever and chronic rheumatic heart diseases, ICD10","Hypertensive diseases, ICD10","Ischaemic heart diseases, ICD10","Cerebrovascular diseases, ICD10","Diseases of arteries, arterioles and capillaries, ICD10","Diseases of the respiratory  system, ICD10","Influenza, ICD10","Pneumonia, ICD10","Chronic lower respiratory diseases, ICD10","Diseases of the digestive system, ICD10","Gastric and duodenal ulcer, ICD10","Diseases of the liver, ICD10","Diseases of the musculoskeletal system and connective tissue, ICD10","Diseases of the genitourinary system, ICD10","Disorders of kidney and ureter, ICD10","Hyperplasia of prostate, ICD10","Certain conditions originating in the perinatal period, ICD10","Congenital malformations, deformations and chromosomal abnormalities , ICD10","Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified, ICD10","External causes, ICD10","Accidents, ICD10","Transport accidents, ICD10","Falls, ICD10","Accidental drowning and submersion, ICD10","Exposure to smoke, fire and flames, ICD10","Accidental poisoning by and exposure to noxious substances, ICD10","Intentional self-harm, ICD10","Assault, ICD10", "Malignant neoplasm of female breast, ICD10", "Malignant neoplasm of cervix uteri, ICD10", "Pregnancy, childbirth and the puerperium, ICD10", "Pregnancy with abortive outcome, ICD10"], "Causa de Muerte", data)
 
 # Generar rangos de edad
-# 0 0-9,1 10-19,2 20-29, 3 30-39,4 40-49,5 50-59,6 60-69, 7 70-79, 8 80-89, 9 90-mas
-
-#data = ConversionCategorizacion(["0", "1", ""])
 # 0 0-9,1 10-19,2 20-29, 3 30-39,4 40-49,5 50-59,6 60-69, 7 70-79, 8 80-89, 9 90-mas
 mat = [["0", "1", "2", "3", "4", "5 - 9"], ["10 - 14", "15 - 19"],
         ["20 - 24", "25 - 29"], ["30 - 34","35 - 39"], ["40 - 44","45 - 49"],
